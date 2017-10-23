d46c7c200c59e5b73688426bb1a9729659306441
Dunya Degirmenci <3137236+ddegirmenci@users.noreply.github.com>
2017-10-23 14:50:50 +0300

    gitignore, setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='HexGrid',
+    url='https://github.com/ddegirmenci/hexgrid/',
+    author='Dünya Değirmenci',
+    author_email='3137236+ddegirmenci@users.noreply.github.com',
+    version='0.1.0',
+    license='MIT License',
+    long_description=open('README.md').read(),
+)
